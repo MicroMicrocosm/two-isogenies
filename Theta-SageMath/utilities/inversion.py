560724086f01bc7a2db3208a396c25b833896b13
MicroMicrocosm <micromicrocosm@foxmail.com>
2024-05-11 22:31:31 +0800

    Replace constant time inversion

--- Theta-SageMath/utilities/inversion.py
+++ Theta-SageMath/utilities/inversion.py
@@ -1,10 +1,20 @@
 def inversion(a):
-    q = a.parent().order()
-    r = bin(q-2)[3:]
+    p = a.parent().characteristic()
+    r = bin(p-2)[3:]
     n = len(r)
-    result = a
-    for i in range(n):
-        result = result ** 2
-        if r[i] == '1':
-            result *= a
+
+    i = a.parent().gen()
+    a_conj = a.conjugate()  # a_conj = x0 - y0*i
+    x = a + a_conj          # x = 2 * x0
+    y = i * (a_conj - a)    # y = 2 * y0
+    
+    b = x * x + y * y
+    result = b
+    for k in range(n):
+        result = result * result
+        if r[k] == '1':
+            result *= b
+    result = result * a_conj
+    result = result + result
+    result = result + result
     return result
